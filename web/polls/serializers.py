6c3b1ee6c1cd4ed4d7d2237efbef1a3f8f7c0b20
Matvey <matvey.kuklev@stemsc.com>
2021-08-13 08:43:00 +0300

    typing

--- web/polls/serializers.py
+++ web/polls/serializers.py
@@ -2,7 +2,7 @@ from django.utils.translation import gettext as _
 from rest_framework import serializers
 from .services import PollsService
 
-error_messages = {
+error_messages: dict = {
     'questions_number': _("the number of responses must be at least 2")
 }
 
@@ -11,12 +11,12 @@ class CreatePollsSerializer(serializers.Serializer):
     question = serializers.CharField()
     choice = serializers.ListSerializer(child=serializers.CharField())
 
-    def validate_choice(self, choice):
+    def validate_choice(self, choice) -> list:
         if len(choice) < 2:
             raise serializers.ValidationError(error_messages['questions_number'])
         return choice
 
-    def save(self, **kwargs):
+    def save(self, **kwargs) -> dict:
         question = PollsService.create_question(self.validated_data['question'])
         PollsService.bulk_create_choice(self.validated_data['choice'], question)
         return self.validated_data
