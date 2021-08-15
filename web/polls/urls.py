36be7f699d456d8ff3dbb013c201a395800f291c
Matvey <matvey.kuklev@stemsc.com>
2021-08-15 21:10:54 +0300

    update

--- web/polls/urls.py
+++ web/polls/urls.py
@@ -10,8 +10,9 @@ router = DefaultRouter()
 urlpatterns = [
     path('create/', views.PollsViewSet.as_view({'post': 'create'}), name='create_polls'),
     path('list/', views.PollsViewSet.as_view({'get': 'list'}), name='list_polls'),
-    path('<pk>/', views.PollsViewSet.as_view({'get': 'retrieve'}), name='list_polls'),
-    path('update/<pk>/', views.PollsViewSet.as_view({'put': 'update'}), name='list_polls'),
+    path('<pk>/', views.PollsViewSet.as_view({'get': 'retrieve'}), name='detail_polls'),
+    path('update/<pk>/', views.PollsViewSet.as_view({'put': 'update'}), name='update_polls'),
+    path('delete/<pk>/', views.PollsViewSet.as_view({'delete': 'destroy'}), name='delete_polls'),
 
 ]
 
--- web/polls/views.py
+++ web/polls/views.py
@@ -2,7 +2,7 @@ import logging
 from django.utils.translation import gettext_lazy as _
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.viewsets import GenericViewSet
-from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework import mixins
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Question, Choice
@@ -12,7 +12,12 @@ from . import serializers
 logger = logging.getLogger(__name__)
 
 
-class PollsViewSet(UpdateModelMixin, RetrieveModelMixin, ListModelMixin, CreateModelMixin, GenericViewSet):
+class PollsViewSet(mixins.UpdateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   mixins.DestroyModelMixin,
+                   GenericViewSet):
 
     def get_serializer_class(self):
         if self.action in ('create', 'update'):
@@ -21,3 +26,9 @@ class PollsViewSet(UpdateModelMixin, RetrieveModelMixin, ListModelMixin, CreateM
 
     def get_queryset(self):
         return Question.objects.prefetch_related('choice_set')
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        Choice.objects.filter(question=instance).delete()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
