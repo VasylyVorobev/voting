bd8be1f0a9c1edc070c4cd4284cc6a8d26941b41
Matvey <matvey.kuklev@stemsc.com>
2021-08-16 21:13:00 +0300

    upd

--- web/polls/urls.py
+++ web/polls/urls.py
@@ -10,11 +10,10 @@ router = DefaultRouter()
 urlpatterns = [
     path('create/', views.PollsViewSet.as_view({'post': 'create'}), name='create_polls'),
     path('list/', views.PollsViewSet.as_view({'get': 'list'}), name='list_polls'),
-    path('<pk>/', views.PollsViewSet.as_view({'get': 'retrieve'}), name='detail_polls'),
     path('update/<pk>/', views.PollsViewSet.as_view({'put': 'update'}), name='update_polls'),
     path('delete/<pk>/', views.PollsViewSet.as_view({'delete': 'destroy'}), name='delete_polls'),
-
     path('get/vote/', views.VotingView.as_view()),
+    path('detail/<pk>/', views.PollsViewSet.as_view({'get': 'retrieve'}), name='detail_polls'),
 
 ]
 
