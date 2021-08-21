05f43d09e98f41568c00b6e4f192104e41a2cb0b
Matvey <matvey.kuklev@stemsc.com>
2021-08-21 16:23:50 +0300

    upd

--- web/polls/views.py
+++ web/polls/views.py
@@ -4,7 +4,6 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.throttling import UserRateThrottle
 from rest_framework import status
 from .models import Question, Choice
 from . import serializers
